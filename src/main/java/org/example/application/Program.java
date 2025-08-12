7d91b6b35d1b8bdd90614369b4d03915660dba2a
Lucas Athié <luathie@gmail.com>
2025-08-12 14:46:55 -0300

    Seller entity class

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -2,12 +2,17 @@ package org.example.application;
 
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "TI");
-        System.out.println(department);
+        Seller seller = new Seller(1, "Lucas", "luathie@gmail.com", new Date(), 3000.00, department);
+
+        System.out.println(seller);
 
     }
 }
